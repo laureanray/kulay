b8ee50f153979cd09ef7b5fef0e35ce871264f97
laureanray <laureanraybahala@gmail.com>
2023-08-12 03:42:20 +0800

    fix colors

--- lua/lush_theme/kulay.lua
+++ lua/lush_theme/kulay.lua
@@ -152,7 +152,7 @@ local theme = lush(function(injected_functions)
     netrwHelpCmd                                                                      { Function }, -- netrwHelpCmd   xxx links to Function
     luaMetaMethod                                                                     { Function }, -- luaMetaMethod  xxx links to Function
     luaFunction                                                                       { Function }, -- luaFunction    xxx links to Function
-    Identifier                                                                        { fg="#cccccc", }, -- Identifier     xxx guifg=#79dac8
+    Identifier                                                                        { fg="#79dac8", }, -- Identifier     xxx guifg=#79dac8
     IdentifierItalic                                                                  { fg="#cccccc", gui = "italic" }, -- Identifier     xxx guifg=#79dac8
     sym"@field"                                                                       { IdentifierItalic }, -- @field         xxx links to Identifier
     NvimIdentifier                                                                    { IdentifierItalic }, -- NvimIdentifier xxx links to Identifier
